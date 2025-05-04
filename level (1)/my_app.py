05c7470cfd9fcd98ce7c6db68635800234efe329
Antimo Qiriazi <antimoqiriazi@gmail.com>
2025-05-04 19:37:41 +0200

    my app

--- level (1)/my_app.py
+++ level (1)/my_app.py
@@ -1,1 +1,26 @@
-# write the code for main app and first screen
+from PyQt5.QtCore import Qt 
+from PyQt5.QtWidgets import QLineEdit, QWidget, QLabel, QApplication, QVBoxLayout, QHBoxLayout
+from instr import*
+from second_win import *
+class MainWin(QWidget):
+    def iniUI(self):
+       #description of interface elements
+       self.hello_text =QLabel(txt_hello)
+       self.instruction =QLabel(txt_instruction)
+       self.button = QPushButton(txt_next)
+       self.layout = QVBoxLayout()
+       self.hello_text.addWidget(self.layout)
+       self.instruction.addWidget(self.layout)
+       self.button.addWidget(self.layout)
+
+    def connects(self):
+        self.btn_next.clicked.connet(self.next_click)
+    def next_click(self):
+       self.hide()  
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
+app = QApplication([])
+mw = MainWin()
+app.exec_()
+
--- level (1)/second_win.py
+++ level (1)/second_win.py
@@ -1,3 +1,1 @@
 # write a code for the second screen of app
-Dyrrah City Football Academy 
-AC Milan
